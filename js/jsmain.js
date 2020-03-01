29bab5cda2994e10e0a04c0eee363a02c8c3edc9
Amaechi Victor <30497549+Viktoh-A@users.noreply.github.com>
2020-03-01 21:55:20 +0100

    first commit

---
+++ js/jsmain.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+// var c = document.getElementById("ddd");
+//     // jQuery methods go here...
+//     console.log("hahahah");
+//     if(c.value = 1){
+//         console.log("dddddd");
+//     }else if(c.value = 2){
+//         console.log("fffffffff")        
+//     }
+//   });
